1ff639f1ec5561add250207b098e7d6bbb77442c
Bard1en <greger.grym@gmail.com>
2024-02-02 12:45:35 +0100

    Add project files.

---
+++ inlämning uppgift 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace inlämning_Uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nummer = new List<int>();
+            nummer.Add(10);
+            nummer.Add(20);
+            nummer.Add(30);
+            nummer.Add(918237);
+            Max(nummer);
+            Console.ReadKey();
+        }
+        static void Max (List<int> nummer)
+        {
+            nummer.Sort();
+            nummer.RemoveRange(0, nummer.Count -1);
+            foreach(int numbers  in nummer)
+            {
+                Console.WriteLine("Det största talet var " + numbers);
+            }
+        }
+    }
+}
